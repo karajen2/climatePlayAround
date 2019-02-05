16e13aa3e4f7f25b1a2443bca00161420c594408
karajen2 <skpkarthik@gmail.com>
2019-02-06 00:08:52 +0530

    Direct project file Upload from GIT

---
+++ Climate/Climate/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows;
+
+namespace Climate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var url = "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22";
+            var webrequest = (HttpWebRequest)System.Net.WebRequest.Create(url);
+
+            using (var response = webrequest.GetResponse())
+            using (var reader = new StreamReader(response.GetResponseStream()))
+            {
+                var result = reader.ReadToEnd();
+                tb.Text = Convert.ToString(result);
+            }
+        }
+    }
+}
